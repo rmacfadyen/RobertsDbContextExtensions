91e5fd041f8f23a833f9122c7d0281d4a8c827f8
Rob MacFadyen <rmacfadyen@rmtrack.com>
2021-12-24 17:14:14 -0500

    Doc

---
+++ RobertsDbContextExtensions/NamespaceDoc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobertsDbContextExtensions
+{
+    /// <summary>
+    /// The original inspiration for these extensions came from ADO.NET's ExecuteScalar(..) method.
+    /// It was clear that with a little generic magic it could have been so much easier to use.
+    /// The same for constructing command objects, just so many of the same steps repeated over
+    /// and over.Data tables are also fairly pondorous, especially when a simple IList&lt;T&gt; would
+    /// be so much easier to deal with.
+    /// </summary>
+    internal static class NamespaceDoc { } // internal so it is not visible outside the assembly
+}
--- docs/RobertsDbContextExtensions.md
+++ docs/RobertsDbContextExtensions.md
@@ -1,4 +1,9 @@
 ## RobertsDbContextExtensions Namespace
+The original inspiration for these extensions came from ADO.NET's ExecuteScalar(..) method.
+It was clear that with a little generic magic it could have been so much easier to use.
+The same for constructing command objects, just so many of the same steps repeated over
+and over.Data tables are also fairly pondorous, especially when a simple IList<T> would
+be so much easier to deal with.
 
 | Classes | |
 | :--- | :--- |
